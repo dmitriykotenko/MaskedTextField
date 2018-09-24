779e0afa33a00cc5ec82b0af8d4d1593003c8e52
Dmitry Kotenko <d.kotenko@yandex.ru>
2018-09-24 20:48:12 +0300

    A couple of UI tests

--- MaskedTextFieldUITests/MaskedTextFieldUITests.swift
+++ MaskedTextFieldUITests/MaskedTextFieldUITests.swift
@@ -6,8 +6,6 @@ class MaskedTextFieldUITests: XCTestCase {
   override func setUp() {
     super.setUp()
     
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    
     // In UI tests it is usually best to stop immediately when a failure occurs.
     continueAfterFailure = false
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
@@ -16,11 +14,24 @@ class MaskedTextFieldUITests: XCTestCase {
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
   }
   
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    super.tearDown()
+  func testThatDecorationEngineWorks() {
+    let app = XCUIApplication()
+    let window = app.windows.element(boundBy: 0)
+    
+    let firstTextField = window.textFields.element(boundBy: 0)
+    firstTextField.tap()
+    
+    app.keys["more"].tap()
+    
+    app.keys["2"].tap()
+    app.keys["3"].tap()
+    app.keys["5"].tap()
+    app.keys["7"].tap()
+    
+    let firstTextFieldValue = firstTextField.value as? String
+    XCTAssert(firstTextFieldValue == "23:57")
   }
-  
+
   func testThatValidationEngineWorks() {
     let app = XCUIApplication()
     let window = app.windows.element(boundBy: 0)
@@ -39,5 +50,33 @@ class MaskedTextFieldUITests: XCTestCase {
     
     let firstTextFieldValue = firstTextField.value as? String
     XCTAssert(firstTextFieldValue == "23:15")
+  }
+  
+  func testThatSanitizationEngineWorks() {
+    let app = XCUIApplication()
+    let window = app.windows.element(boundBy: 0)
+    
+    let firstTextField = window.textFields.element(boundBy: 0)
+    firstTextField.tap()
+    
+    app.keys["more"].tap()
+    
+    app.keys["2"].tap()
+    app.keys["3"].tap()
+    app.keys["space"].tap()
+    app.keys["?"].tap()
+    app.keys["4"].tap()
+    app.keys["7"].tap()
+    app.keys["space"].tap()
+
+    app.keys["more"].tap()
+    app.keys["x"].tap()
+    app.keys["d"].tap()
+    
+    app.keys["more"].tap()
+    app.keys["!"].tap()
+
+    let firstTextFieldValue = firstTextField.value as? String
+    XCTAssert(firstTextFieldValue == "23:47")
   }
 }
